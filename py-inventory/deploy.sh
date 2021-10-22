1678ad00e49f131bc3d4b907e2f6716c346f30ca
Jiangjunyu <double.chiang@gmail.com>
2021-10-22 14:30:16 -0700

    fixes pip install SSL error

--- py-inventory/deploy.sh
+++ py-inventory/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 yum install -y python3
-pip3 install -r /mnt/smbfs/py-inventory/requirements.txt
+# ssl error correction, https://stackoverflow.com/questions/49324802/pip-always-fails-ssl-verification
+pip3 install -r /mnt/smbfs/py-inventory/requirements.txt  --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org
 python3 /mnt/smbfs/py-inventory/inventory.py --target http://10.16.0.1/inventories/create
